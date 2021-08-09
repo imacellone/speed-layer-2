67ac09f5728e9f36307abe7667c8a15fd5e600b0
italo macellone <italomacellone@gmail.com>
2021-08-10 01:31:11 +0200

    saving to mongo, needs testing

---
+++ streaming/script/streaming.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "Preparing..."
+
+input="/data/input/adult.data"
+output="/data/output/output.data"
+
+sleep_time_milliseconds=0.004
+
+echo "Cleaning environment..."
+
+# If the output file exists, delete it.
+if test -f "$output"; then
+    rm -rf $output
+fi
+
+echo ">>>>>>>>> STREAMING <<<<<<<<<"
+
+# Writes one line of the input file to the output file, every sleep_time_seconds seconds.
+while IFS= read -r line
+do
+    echo "$line" >> $output
+    sleep $sleep_time_milliseconds
+done < "$input"
+
+echo "Success!"
