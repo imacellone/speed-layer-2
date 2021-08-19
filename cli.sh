f19b5a7ff5f44b4fd93ad8a397255221d909c7d2
italo macellone <italomacellone@gmail.com>
2021-08-19 23:18:16 +0200

    cleanup of cli

--- cli.sh
+++ cli.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#mongo.sh
-#rm-containers.sh
-#rm-persisted.sh
-#setup.sh
-#start.sh
-#tail-streaming.sh
-#exit
-
 menu_text() {
     echo -e "\n\n***************************************"
     echo -e "********** POC - SPEED LAYER **********"
@@ -15,11 +7,12 @@ menu_text() {
     echo -e "Options:"
     echo -e "\n1 - Setup Environment.\n"
     echo -e "2 - Start."
-    echo -e "3 - Tail Streaming. (WARNING: This will quit this client. To stop tailing press CTRL-C )"
-    echo -e "4 - Remove all Containers."
-    echo -e "5 - Remove all Persisted Data"
-    echo -e "6 - Get Containers' Statuses"
-    echo -e "\n0 - Exit (Closes this client. No containers and/or data will be removed)"
+    echo -e "3 - Application Status."
+    echo -e "4 - Tail Streaming. (WARNING: This will quit this client. To stop tailing press CTRL-C)."
+    echo -e "5 - Open MongoDB Conteiner. (To quit the MongoDB container, press CTRL-D)."
+    echo -e "6 - Remove all Containers."
+    echo -e "7 - Remove all Persisted Data."
+    echo -e "\n0 - Exit (Closes this client. No containers and/or data will be removed)."
 }
 
 show_menu() {
@@ -37,13 +30,15 @@ do
     elif [ "$option" = "2" ]; then
         start
     elif [ "$option" = "3" ]; then
-        tail_streaming
-    elif [ "$option" = "4" ]; then
-        remove_containers
-    elif [ "$option" = "5" ]; then
-        remove_persisted
-    elif [ "$option" = "6" ]; then
         get_status
+    elif [ "$option" = "4" ]; then
+        tail_streaming
+    elif [ "$option" = "5" ]; then
+        open_mongo_container
+    elif [ "$option" = "6" ]; then
+        remove_containers
+    elif [ "$option" = "7" ]; then
+        remove_persisted
     elif [ "$option" = "0" ]; then
         exit
     else
@@ -53,7 +48,7 @@ done
 }
 
 press_any_key() {
-    echo -e "\nPress any key to continue."
+    echo -e "\nPress any key to continue..."
     read -n 1 -s
 }
 
@@ -70,9 +65,20 @@ start() {
     press_any_key
 }
 
+get_status() {
+    clear
+    sudo docker-compose ps
+    press_any_key
+}
+
 tail_streaming() {
     clear
     ./tail-streaming.sh
+}
+
+open_mongo_container() {
+    clear
+    ./mongo.sh
 }
 
 remove_containers() {
@@ -86,12 +92,6 @@ remove_persisted() {
     clear
     ./rm-persisted.sh
     echo -e "\nAll persisted data has been removed."
-    press_any_key
-}
-
-get_status() {
-    clear
-    sudo docker-compose ps
     press_any_key
 }
 
